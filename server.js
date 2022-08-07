5655d46aa6432dc632ef306462abfe38c301f85b
Mina Ghaly <minanabil@msn.com>
2022-08-07 13:50:43 +1000

    server code added

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require("path");
+var app = express();
+var PORT = 5500;
+// console.log(path.resolve("/public")); 
+// Single routing
+   
+app.use(express.static("./Develop/public"));
+// app.get('/notes', function (req, res, next) {
+//     // res.send("hello");
+//     res.redirect("http://localhost:5501/notes.html");
+// })
+// app.use('/static', express.static('Develop'));
+// app.use(express.static('public'));
+
+// app.use(express.json());
+app.use(express.static('Develop/public'));
+app.get("/notes",(req,res,next)=>res.redirect("notes.html"));
+// app.use((req,res,next)=>res.redirect("notes.html"));
+
+// app.get("*",(req,res,next)=>res.send("aaaqqaa"));
+
+ 
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
