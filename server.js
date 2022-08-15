f93b45c1f40ee3e98b6d4fcf59f390bbc6bb46e9
Mina Ghaly <minanabil@msn.com>
2022-08-15 20:29:42 +0200

    port fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = 5502;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const app = express();
 // Helper method for generating unique ids
